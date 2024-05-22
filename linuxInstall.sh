bdab875eb2492eda608396043582737a44af024b
Mykyta Berezhnyi <nikber.dp@gmail.com>
2024-05-22 19:44:36 +0300

    update nvm install

--- linuxInstall.sh
+++ linuxInstall.sh
@@ -3,4 +3,5 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosugges
 script_dir=$(dirname "$(readlink -f "$0")")
 cp $script_dir/.zshrc ~/
 cp $script_dir/.p10k.zsh ~/
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 chsh -s $(which zsh)
--- macInstall.sh
+++ macInstall.sh
@@ -2,3 +2,4 @@ brew install mc micro btop tmux fzf zoxide eza ffmpeg ncdu rsync speedtest-cli f
 script_dir=$(dirname "$(readlink -f "$0")")
 cp $script_dir/.zshrc ~/
 cp $script_dir/.p10k.zsh ~/
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
